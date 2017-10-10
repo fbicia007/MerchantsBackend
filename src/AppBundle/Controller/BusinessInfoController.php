73c039d499a1724611977c1e8f52d867b45a5916
Xu Wang <fbicia@XudeiMac.local>
2017-10-10 22:27:09 +0200

    add shop info elements take parameter in Templet

--- src/AppBundle/Controller/BusinessInfoController.php
+++ src/AppBundle/Controller/BusinessInfoController.php
@@ -4,12 +4,15 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Shop;
 use AppBundle\Entity\Country;
+use AppBundle\Entity\Type;
 use AppBundle\Form\BrandType;
+use AppBundle\Form\OpeningType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
+
 
 class BusinessInfoController extends Controller
 {
@@ -21,6 +24,20 @@ class BusinessInfoController extends Controller
         $user = $this->getUser();
         $userName = $user->getusername();
         $userEmail = $user->getemail();
+        $userId = $user->getid();
+
+        $shop = new Shop();
+
+        $shop->setUser($userId);
+
+
+        $shop->setType();
+        $shop->setCouponCode('coupon');
+        $shop->setBrands('brands');
+        $shop->setThumbnail('thumb');
+        $shop->setPictures('picture');
+        $shop->setTaxRefund(1);
+        //$shop->setSuffix();
 
 
         $country_obj = $this->getDoctrine()
@@ -43,11 +60,11 @@ class BusinessInfoController extends Controller
 
 
 
-        $form = $this->createFormBuilder()
-            ->add('company_name', TextType::class,[
+        $form = $this->createFormBuilder($shop)
+            ->add('company', TextType::class,[
                 'attr'=>['placeholder'=>'Company Name'],
             ])
-            ->add('store_name', TextType::class,[
+            ->add('name', TextType::class,[
                 'attr'=>['placeholder'=>'Store Name'],
             ])
             ->add('manager_title', ChoiceType::class, [
@@ -75,6 +92,19 @@ class BusinessInfoController extends Controller
                 'choices'=> $country,
                 'attr'=>['class'=>'form-control','style'=>'margin: 0 0 2% 0;'],
             ])
+            ->add('tel', TextType::class,[
+                'attr'=>['class'=>'form-control','style'=>'width:49%; display:inline-block;','placeholder'=>'Telephone'],
+            ])
+            ->add('email', TextType::class,[
+                'attr'=>['class'=>'form-control','style'=>'width:49%; margin: 0 0 2% 2%; display:inline-block;','placeholder'=>'E-Mail'],
+            ])
+            ->add('fax', TextType::class,[
+                'attr'=>['class'=>'form-control','style'=>'width:49%; display:inline-block;','placeholder'=>'Fax'],
+            ])
+            ->add('web', TextType::class,[
+                'attr'=>['class'=>'form-control','style'=>'width:49%; margin: 0 0 2% 2%; display:inline-block;','placeholder'=>'Homepage'],
+            ])
+            ->add('opening_hours', OpeningType::class)
             ->getForm();
 
 
@@ -85,11 +115,13 @@ class BusinessInfoController extends Controller
         if ($form->isSubmitted() && $form->isValid()){
 
             dump($form->getData());
+
+
             $em = $this->getDoctrine()->getManager();
 
-            $brand = $form->getData();
+            //$shop = $form->getData();
 
-            $em->persist($brand);
+            $em->persist($shop);
             $em->flush();
 
         }
--- src/AppBundle/Form/OpeningType.php
+++ src/AppBundle/Form/OpeningType.php
@@ -11,9 +11,118 @@ class OpeningType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name',TextType::class)
-            ->add('nameCn', TextType::class)
-            ->add('nickname', TextType::class);
+        $builder->add('mo_am',TextType::class,[
+            'attr'=>[
+                'class'=>'form-control',
+                'style'=>'width:48%; display:inline-block;',
+                'type'=>'time',
+                'id'=>'mo_am',
+                'value'=>'08:00'],
+        ])
+            ->add('mo_pm', TextType::class,[
+                'attr'=>[
+                    'class'=>'form-control',
+                    'style'=>'width:47%; display:inline-block;',
+                    'type'=>'time',
+                    'id'=>'mo_pm',
+                    'value'=>'20:00'],
+            ])
+            ->add('tu_am', TextType::class,[
+                'attr'=>[
+                    'class'=>'form-control',
+                    'style'=>'width:48%; display:inline-block;',
+                    'type'=>'time',
+                    'id'=>'tu_am',
+                    'value'=>'08:00'],
+            ])
+            ->add('tu_pm', TextType::class,[
+                'attr'=>[
+                    'class'=>'form-control',
+                    'style'=>'width:47%; display:inline-block;',
+                    'type'=>'time',
+                    'id'=>'tu_pm',
+                    'value'=>'20:00'],
+            ])
+            ->add('wed_am', TextType::class,[
+                'attr'=>[
+                    'class'=>'form-control',
+                    'style'=>'width:48%; display:inline-block;',
+                    'type'=>'time',
+                    'id'=>'wed_am',
+                    'value'=>'08:00'],
+            ])
+            ->add('wed_pm', TextType::class,[
+                'attr'=>[
+                    'class'=>'form-control',
+                    'style'=>'width:47%; display:inline-block;',
+                    'type'=>'time',
+                    'id'=>'wed_pm',
+                    'value'=>'20:00'],
+            ])
+            ->add('thu_am', TextType::class,[
+                'attr'=>[
+                    'class'=>'form-control',
+                    'style'=>'width:48%; display:inline-block;',
+                    'type'=>'time',
+                    'id'=>'thu_am',
+                    'value'=>'08:00'],
+            ])
+            ->add('thu_pm', TextType::class,[
+                'attr'=>[
+                    'class'=>'form-control',
+                    'style'=>'width:47%; display:inline-block;',
+                    'type'=>'time',
+                    'id'=>'thu_pm',
+                    'value'=>'20:00'],
+            ])
+            ->add('fr_am', TextType::class,[
+                'attr'=>[
+                    'class'=>'form-control',
+                    'style'=>'width:48%; display:inline-block;',
+                    'type'=>'time',
+                    'id'=>'fr_am',
+                    'value'=>'08:00'],
+            ])
+            ->add('fr_pm', TextType::class,[
+                'attr'=>[
+                    'class'=>'form-control',
+                    'style'=>'width:47%; display:inline-block;',
+                    'type'=>'time',
+                    'id'=>'fr_pm',
+                    'value'=>'20:00'],
+            ])
+            ->add('sa_am', TextType::class,[
+                'attr'=>[
+                    'class'=>'form-control',
+                    'style'=>'width:48%; display:inline-block;',
+                    'type'=>'time',
+                    'id'=>'sa_am',
+                    'value'=>'08:00'],
+            ])
+            ->add('sa_pm', TextType::class,[
+                'attr'=>[
+                    'class'=>'form-control',
+                    'style'=>'width:47%; display:inline-block;',
+                    'type'=>'time',
+                    'id'=>'su_am',
+                    'value'=>'20:00'],
+            ])
+            ->add('su_am', TextType::class,[
+                'attr'=>[
+                    'class'=>'form-control',
+                    'style'=>'width:48%; display:inline-block;',
+                    'type'=>'time',
+                    'id'=>'su_pm',
+                    'value'=>'08:00'],
+            ])
+            ->add('su_pm', TextType::class,[
+                'attr'=>[
+                    'class'=>'form-control',
+                    'style'=>'width:47%; display:inline-block;',
+                    'type'=>'time',
+                    'id'=>'su_pm',
+                    'value'=>'20:00'],
+            ]);
 
     }
 
